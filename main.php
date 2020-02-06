453cff11096ed55a23f1f4d801389d6ae80a1d8a
ZZIIKKemd <ZZIIKKemd@users.noreply.github.com>
2020-02-06 20:01:25 +0300

    Create main.php
    Решил унаследовать менеджера и админа от пользователя, в котором прописал базовую логику. Методы canEdit и canDelete заменяются при необходимости в наследниках.
    Также привёл базовую реализацию класса Message (сообщение). В этой реализации сообщения создаются от пользователя, поэтому идентификатор пользователя жёстко прописывается в нём автоматически.

---
+++ main.php
@@ -0,0 +1,117 @@
+<?php
+
+/**
+* Пользователь
+* Может редактировать только свои сообщения
+* Удалять сообщения не может
+* Class User
+*/
+class User
+{
+	/** @var int */
+	private $id;
+	
+	/** @var string */
+    private $name;
+	
+	function __construct(int $id, string $name)
+	{
+		$this -> id = $id;
+		$this -> name = $name;
+	}
+	
+	/**
+	* Проверяет, может ли пользователь редактировать сообщение
+	* @param Message $message
+	* @return bool
+	*/
+	public function canEdit(Message $message): bool
+	{
+		if ($message->getCreator()->getID() == $this->id):
+			return true;
+		else:
+			return false;
+		endif;
+	}
+
+	/**
+	* Проверяет, может ли пользователь удалить сообщение
+	* @param Message $message
+	* @return bool
+	*/
+	public function canDelete(Message $message): bool
+	{
+		return false;
+	}
+	
+	/**
+	* Возвращает id пользователя
+	* @return int
+	*/
+	public function getID(): int
+	{
+		return $this -> id;
+	}
+	
+	/**
+	* Создаёт новое сообщение от лица пользователя
+	* @param Message $text
+	* @return Message
+	*/
+	public function newMessage(string $text)
+	{
+		return new Message($this, $text);
+	}
+}
+
+/**
+* Менеджер
+* Может редактировать свои сообщения и собщения других пользователей
+* Удалять сообщения не может
+* Class Manager
+*/
+class Manager extends User
+{
+ public function canEdit(Message $message): bool
+	{
+		return true;
+	}
+}
+
+/**
+* Администратор
+* Может редактировать и удалять любые сообщения
+* Class Admin
+*/
+class Admin extends User
+{
+	public function canEdit(Message $message): bool
+	{
+		return true;
+	}
+
+	public function canDelete(Message $message): bool
+	{
+		return true;
+	}
+}
+
+class Message
+{
+    /** @var User */
+    private $creator;
+    
+    /** @var string */
+    private $text;
+    
+    public function __construct(User $creator, string $text)
+	{
+		$this -> creator = $creator;
+		$this -> text = $text;
+	}
+    
+	public function getCreator(): User
+	{
+		return $this -> creator;
+	}
+}
